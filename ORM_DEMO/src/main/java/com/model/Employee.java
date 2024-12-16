6b75195055acdd97d4b9087167e3f022c0aa5182
beinit7799 <100144161+beinit7799@users.noreply.github.com>
2024-12-16 10:06:50 +0545

    Many to one

--- ORM_DEMO/src/main/java/com/model/Employee.java
+++ ORM_DEMO/src/main/java/com/model/Employee.java
@@ -1,9 +1,12 @@
 package com.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
@@ -18,8 +21,17 @@ public class Employee {
 	@OneToOne
 	@JoinColumn(name = "adrId")//FK
 	private Address address;
+	@OneToMany(mappedBy = "employee")
+	private List<Phone> phoneList;
+	
 	public int getId() {
 		return id;
+	}
+	public List<Phone> getPhoneList() {
+		return phoneList;
+	}
+	public void setPhoneList(List<Phone> phoneList) {
+		this.phoneList = phoneList;
 	}
 	public void setId(int id) {
 		this.id = id;
--- ORM_DEMO/src/main/java/com/model/MappingTest.java
+++ ORM_DEMO/src/main/java/com/model/MappingTest.java
@@ -1,12 +1,15 @@
 package com.model;
 
+import java.util.Arrays;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class MappingTest {
 	public static void main(String[] args) {
-		oneToone();
+		//oneToone();
+		oneTomany();
 	}
 	
 	//oneToone
@@ -31,7 +34,42 @@ public class MappingTest {
 		session.save(emp);
 		session.getTransaction().commit();
 		session.close();
+	}
+	//one to many
+	static void oneTomany() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
 		
+		Address adr = new Address();
+		adr.setCountry("Nepal");
+		adr.setCity("ktm");
+		adr.setState("Bagmati");
+		
+		session.save(adr);
+		
+		Employee emp = new Employee();
+		emp.setFname("Roman");
+		emp.setLname("Dhami");
+		emp.setSalary(9000);
+		emp.setAddress(adr);
+		
+		Phone p1 = new Phone();
+		p1.setNumber("9841214232");
+		p1.setType("Ncell");
+		p1.setEmployee(emp);
+		session.save(p1);
+		
+		Phone p2 = new Phone();
+		p2.setNumber("9876542123");
+		p2.setType("NTC");
+		p2.setEmployee(emp);
+		session.save(p2);
+		
+		emp.setPhoneList(Arrays.asList(p1,p2));
+		session.save(emp);
+		session.getTransaction().commit();
+		session.close();
 		
 	}
 }
