231b5be1795f1fd6c6c65266cf165684931c037d
beinit7799 <100144161+beinit7799@users.noreply.github.com>
2024-12-16 09:26:42 +0545

    created pojo

---
+++ ORM_DEMO/src/main/java/com/model/Employee.java
@@ -0,0 +1,55 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "employee")
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String fname;
+	private String lname;
+	private int salary;
+	@OneToOne
+	@JoinColumn(name = "adrId")//FK
+	private Address address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+
+}
