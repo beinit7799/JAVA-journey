d50caa3c9220decf21f9fa227e696fb05ff4d0ec
beinit7799 <100144161+beinit7799@users.noreply.github.com>
2024-12-19 07:26:05 +0545

    login to home directed

--- springbootdemo/src/main/java/com/bway/springdemo/controller/LoginController.java
+++ springbootdemo/src/main/java/com/bway/springdemo/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.bway.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bway.springdemo.model.User;
 
 @Controller
 public class LoginController {
@@ -9,6 +14,17 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLogin() {
 		
+		return "LoginForm";
+	}
+	
+	@PostMapping("/login")
+	public String postLoigin(@ModelAttribute User u , Model model ) {  //form ko post gareko data aauxa
+		
+		if(u.getUsername().equals("hari") && u.getPassword().equals("123")) {
+			model.addAttribute("uname",u.getUsername());
+			return "Home";
+		}
+		model.addAttribute("message","user do not exist");
 		return "LoginForm";
 	}
 	
