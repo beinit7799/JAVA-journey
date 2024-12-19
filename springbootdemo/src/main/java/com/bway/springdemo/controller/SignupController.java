e7f2fc1ea4fb6d5ce34fc032e465b28d9c97284f
beinit7799 <100144161+beinit7799@users.noreply.github.com>
2024-12-19 07:52:11 +0545

    Signup data viewed to profile

--- springbootdemo/src/main/java/com/bway/springdemo/controller/SignupController.java
+++ springbootdemo/src/main/java/com/bway/springdemo/controller/SignupController.java
@@ -1,7 +1,12 @@
 package com.bway.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bway.springdemo.model.User;
 
 @Controller
 public class SignupController {
@@ -11,6 +16,16 @@ public class SignupController {
 		
 		return "SignupForm";
 	}
-	
+	@PostMapping("/signup")
+	public String postSignup(@ModelAttribute User u , Model model ) {
+		
+		model.addAttribute("fname",u.getFname());
+		model.addAttribute("lname", u.getLname());
+		model.addAttribute("uname",u.getUsername());
+		model.addAttribute("password", u.getPassword());
+		
+		
+		return "Profile";
+	}
 
 }
