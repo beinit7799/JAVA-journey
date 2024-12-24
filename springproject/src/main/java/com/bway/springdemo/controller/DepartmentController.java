592cfc5b4707ed7a66e7f6b419397543bdf9d970
beinit7799 <100144161+beinit7799@users.noreply.github.com>
2024-12-24 19:04:47 +0545

    delete edit and update added

--- springproject/src/main/java/com/bway/springdemo/controller/DepartmentController.java
+++ springproject/src/main/java/com/bway/springdemo/controller/DepartmentController.java
@@ -2,10 +2,12 @@ package com.bway.springdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bway.springdemo.model.Department;
 import com.bway.springdemo.service.DepartmentService;
@@ -29,5 +31,30 @@ public class DepartmentController {
 		
 		return "DepartmentForm";
 	}
-
+	@GetMapping("/list")
+	public String getAll(Model model) {
+		model.addAttribute("dlist",deptService.getAllDepts());
+		return "DepartmentListForm";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam("id") int id) {
+		
+		deptService.deleteDept(id);
+		
+		return"redirect:list";
+	}
+	@GetMapping("/edit")
+	public String edit(@RequestParam("id") int id , Model model) {
+		
+		model.addAttribute("dModel",deptService.getDeptById(id));
+		
+		return"DepartmentEditForm";
+	}
+	@PostMapping("/update")
+	public String update(@ModelAttribute Department dept) {
+		
+		deptService.updateDept(dept);
+		return "redirect:list";
+	}
+	
 }
